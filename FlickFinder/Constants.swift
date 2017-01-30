0816468017a668f6bd734b26240ab1c5fe56bff3
hiroolt <sunasuna.8@gmail.com>
2017-01-30 17:18:56 +0200

    First Commit

--- FlickFinder/Constants.swift
+++ FlickFinder/Constants.swift
@@ -41,7 +41,7 @@ struct Constants {
     // MARK: Flickr Parameter Values
     struct FlickrParameterValues {
         static let SearchMethod = "flickr.photos.search"
-        static let APIKey = "YOUR_API_KEY_HERE"
+        static let APIKey = "b92df0d22ffcea065e71fe270ceff223"
         static let ResponseFormat = "json"
         static let DisableJSONCallback = "1" /* 1 means "yes" */
         static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
